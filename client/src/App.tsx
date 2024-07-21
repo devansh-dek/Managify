328eaae47795023ffd08378297077143fa089015
Agnish1611 <sukla.baruipur2@gmail.com>
2024-07-21 16:28:24 +0530

    feat: Added sections

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,18 @@
 import { Suspense } from "react";
-import GoogleCalendar from "./features/Calendar/GoogleCalendar.tsx";
-import Sidebar from "./sections/Sidebar.tsx";
-import SlideSection from "./features/Slides/SlideSection.tsx";
-
-import PersonalSection from "./sections/PersonalSection.tsx";
-import CompanySection from "./sections/CompanySection.tsx";
+import Sidebar from "./sections/Sidebar";
+import HeadingSection from "./sections/HeadingSection";
+import PersonalSection from "./sections/PersonalSection";
+import CompanySection from "./sections/CompanySection";
 
 function App() {
   return (
     <div className="h-screen w-screen flex overflow-x-hidden bg-[#292c3c] gap-2">
       <Sidebar />
-
+      <div className="pl-[400px] w-screen">
+        <HeadingSection />
+        <PersonalSection />
+        <CompanySection />
+      </div>
     </div>
   );
 }
